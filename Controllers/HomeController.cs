05b8f0cb58795ce8eb9ef0fd1738aac383b0a0fe
Jesse Bryan and Jonathan Stein <jesse+jonathan@>
2017-08-01 15:22:48 -0700

    car dealer updated

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using CarDealer.Models;
 using System.Collections.Generic;
+using System;
 
 namespace CarDealer.Controllers
 {
@@ -17,9 +18,17 @@ namespace CarDealer.Controllers
         {
             Car myCar = new Car();
             myCar.SetMakeModel(Request.Form["make-model"]);
-            myCar.SetMileage(int.Parse(Request.Form["mileage"]));
-            myCar.SetPrice(float.Parse(Request.Form["price"]));
+            if ((Request.Form["mileage"]).ToString().Length > 0 && (Request.Form["price"]).ToString().Length > 0)
+            {
+                myCar.SetMileage(int.Parse(Request.Form["mileage"]));
+                myCar.SetPrice(float.Parse(Request.Form["price"]));
+            }
+            else
+            {
+                //throw error
+            }
             myCar.Save();
+
             return View(Car.AllCars());
         }
 
